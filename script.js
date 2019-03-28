118fbfc78bc314d3263a289a9566273b1acc1ba4
Marissa Polascak <marissapolascak@MB-VIDEO-MP.local>
2019-03-28 19:45:07 -0400

    mobile products

---
+++ script.js
@@ -0,0 +1,45 @@
+var redShades = {
+  title: 'Red',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-red.jpg',
+  price: 99
+};
+var greenShades = {
+  title: 'Green',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-green.jpg',
+  price: 99
+};
+var blueShades = {
+  title: 'Blue',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-blue.jpg',
+  price: 99
+};
+var blackShades = {
+  title: 'Black',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-black.jpg',
+  price: 99
+};
+
+var shades = [redShades, greenShades, blueShades, blackShades];
+
+
+function mobileProducts(product) {
+  return `
+  <div class="col peppers-product text-center">
+    <image class="img-fluid" src="${product.img}" alt=Peppers Rock-N-Stroll Red">
+    <p class="product-title">${product.subtitle}</p>
+    <h3>${product.title}</h3>
+    <p class="product-price">$${product.price}</p>
+    <div class="product-cta-container">
+      <a href="#" class="btn btn-danger btn-lg">Add to Cart</a>
+    </div>
+  </div>
+  `;
+}
+
+$(document).ready(function() {
+  $('#mobile-products').html(mobileProducts(blueShades));
+});

ff64b09344e851f04eec55d35c5c06030fe235c8
Marissa Polascak <marissapolascak@MB-VIDEO-MP.local>
2019-03-28 20:17:55 -0400

    circles

--- script.js
+++ script.js
@@ -32,6 +32,12 @@ function mobileProducts(product) {
     <image class="img-fluid" src="${product.img}" alt=Peppers Rock-N-Stroll Red">
     <p class="product-title">${product.subtitle}</p>
     <h3>${product.title}</h3>
+    <div class="circles">
+      <div class="circle red"></div>
+      <div class="circle green"></div>
+      <div class="circle blue"></div>
+      <div class="circle black"></div>
+    </div>
     <p class="product-price">$${product.price}</p>
     <div class="product-cta-container">
       <a href="#" class="btn btn-danger btn-lg">Add to Cart</a>
